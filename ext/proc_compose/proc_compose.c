68af755afacc5ced0f0ee6384dd24bb33d3e5a6d
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-10-02 18:08:02 -0400

    cruft

--- ext/proc_compose/proc_compose.c
+++ ext/proc_compose/proc_compose.c
@@ -17,18 +17,12 @@
 
 #include <ruby.h>
 
-// Defining a space for information and references about the module to be stored internally
-VALUE Compose = Qnil;
-
-// Prototype for the initialization method - Ruby calls this, not you
 void Init_compose();
 
 VALUE compose(VALUE arg, VALUE procs) {
 	VALUE first = rb_ary_entry(procs, 0);
 	VALUE second = rb_ary_entry(procs, 1);
 
-	// allocates too many intermediate arrays
-	//return rb_proc_call(second, rb_ary_new3(1, rb_proc_call(first, rb_ary_new3(1, arg))));
 	VALUE first_result = rb_funcall(first, rb_intern("call"), 1, arg);
 	VALUE final_result = rb_funcall(second, rb_intern("call"), 1, first_result);
 
@@ -40,13 +34,9 @@ VALUE method_compose(VALUE self, VALUE other_proc) {
 	procs = rb_ary_push(procs, self);
 	VALUE result = rb_proc_new(compose, procs);
 
-	//	FILE *f = fopen("clog.txt", "w");
-	//	fprintf(f, "procs: %s\n", RSTRING_PTR(rb_ary_to_s(procs)));
-
 	return result;
 }
 
-// The initialization method for this module
 void Init_compose() {
 	rb_define_method(rb_cProc, "compose", method_compose, -2);
 	rb_define_method(rb_cProc, "*", method_compose, -2);
